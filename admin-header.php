2e87b5fbaf94be3f0f5037caf09d92fbdab142d7
dmyr0613 <dmyr0613@gmail.com>
2020-08-23 20:48:01 +0900

    1

--- admin-header.php
+++ admin-header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>CallOnCall | 管理者画面</title>
+<title>CommuTalk | 管理者画面</title>
 <link rel="shortcut icon" type="image" href="images/favicon.ico">
 <meta http-equiv="Content-Type" content="text/html; charset=shift_jis">
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-<link rel="stylesheet" href="assets/css/admin-main0.css" />
+<link rel="stylesheet" href="assets/css/main.css" />
 </head>
 
 <?php
