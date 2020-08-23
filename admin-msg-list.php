3fcd5192c3556b5ea2074613482637da66d4d006
dmyr0613 <dmyr0613@gmail.com>
2020-08-23 20:50:55 +0900

    1

---
+++ admin-msg-list.php
@@ -0,0 +1,58 @@
+<?php session_start(); ?>
+<?php require 'admin-header.php'; ?>
+
+<!-- Main -->
+	<div id="main">
+		<div class="inner">
+
+				<!-- Header -->
+				<?php require 'admin-header-sub.php'; ?>
+
+				<!-- reserveMain -->
+				<section id="reserveMain">
+					<?php
+						$msg_no = 0;
+
+						// デバイス情報を取得する。
+						$sql=$pdo->prepare('select * from call_message order by msg_no');
+						$sql->execute();
+
+						echo '<form action="admin-msg-send.php" method="post">';				//送信用のpost
+						echo '<table>';
+						echo '<th width=25>メッセージNo</th><th>メッセージ</th><th width=20>修正</th><th width=20>削除</th>';
+						foreach ($sql as $row) {
+							$msg_no = $row['msg_no'];
+							echo '<tr>';
+							echo '<td>', $msg_no, '</td>';
+							// 一意にするため、nameにデバイス名を付加する。　
+							echo '<td><input type="text" name="msg_no_' . $msg_no . '" value="', $row['message'], '"></td>';
+
+							echo '<td>';
+							echo '<input type="checkbox" id="mod_' . $msg_no . '" name="mod_' . $msg_no . '">';
+							echo '<label for="mod_' . $msg_no . '"> </label>';
+							echo '</td>';
+							echo '<td>';
+							echo '<input type="checkbox" id="del_' . $msg_no . '" name="del_' . $msg_no . '">';
+							echo '<label for="del_' . $msg_no . '"> </label>';
+							echo '</td>';
+						}
+						//追加用のレコード
+						$msg_no = $msg_no + 1;
+						echo '<tr>';
+						// echo '<td>', $msg_no, '</td>';
+						echo '<td><input type="number" name="ins_no" value="' . $msg_no . '" class="input_number_only"></td>';
+						echo '<td><input type="text" name="ins_msg" value="" ></td>';
+						echo '<td>　</td>';
+						echo '<td>　</td>';
+						echo '</table>';
+						echo '<input type="submit" class="button primary" value="Register">';
+						echo '</form>';
+
+					?>
+				</section>
+
+			</div>
+		</div>
+
+<?php require 'admin-menu.php'; ?>
+<?php require 'footer.php'; ?>
---
+++ admin-msg-send.php
@@ -0,0 +1,103 @@
+<?php session_start(); ?>
+<?php require 'admin-header.php'; ?>
+
+<!-- Main -->
+	<div id="main">
+		<div class="inner">
+
+				<!-- Header -->
+				<?php require 'admin-header-sub.php'; ?>
+
+				<!-- messageSend -->
+				<section id="messageSend">
+					<?php
+
+          if (!empty($_REQUEST)) {
+						$obj = $_REQUEST;
+						error_log(print_r($obj, true));
+
+						foreach ($obj as $key => $val){
+							// error_log($key);
+							// if (substr_count($key, 'msg_no_') == 1) {
+							// 	//文字列にmsg_noが含まれる場合
+							// 	$msg_no = substr($key, 7);	//メッセージNoを抜き取る。
+							// 	error_log($msg_no);
+							// 	$message = $_REQUEST['msg_no_' . $msg_no];
+							// 	error_log($message);
+							//
+							// 	if ($msg_no > 0 && $message != "") {
+							// 		error_log("メッセージ更新");
+							// 		//webAPIにて、メッセージ更新チェック
+							// 		$url = "http://calloncall.herokuapp.com/calling-msg-check.php?msg_no=" . $msg_no . "&message=" . $message;
+							// 		$ch = curl_init();
+							// 		curl_setopt($ch, CURLOPT_URL, $url);
+							//
+							// 		$response = curl_exec($ch);
+							// 		curl_close($ch);
+							// 		error_log(print_r($response, true));
+							// 	}
+							// }
+
+							if (substr_count($key, 'mod_') == 1) {
+								//文字列にmod_が含まれる場合は修正
+								$msg_no = substr($key, 4);	//メッセージNoを抜き取る。
+								error_log("修正メッセージNo." . $msg_no);
+								$message = $_REQUEST['msg_no_' . $msg_no];
+								error_log($message);
+
+								if ($msg_no > 0 && $message != "") {
+									//webAPIにて、メッセージ更新チェック
+									$url = "http://calloncall.herokuapp.com/calling-msg-check.php?msg_no=" . $msg_no . "&message=" . $message;
+									$ch = curl_init();
+									curl_setopt($ch, CURLOPT_URL, $url);
+
+									$response = curl_exec($ch);
+									curl_close($ch);
+									error_log(print_r($response, true));
+								}
+							} elseif (substr_count($key, 'del_') == 1) {
+								//文字列にmod_が含まれる場合は削除
+								$msg_no = substr($key, 4);	//メッセージNoを抜き取る。
+								error_log("削除メッセージNo." . $msg_no);
+								//webAPIにて、メッセージ更新チェック
+								$url = "http://calloncall.herokuapp.com/calling-msg-delete.php?msg_no=" . $msg_no;
+								$ch = curl_init();
+								curl_setopt($ch, CURLOPT_URL, $url);
+
+								$response = curl_exec($ch);
+								curl_close($ch);
+								error_log(print_r($response, true));
+							} elseif (substr_count($key, 'ins_no') == 1) {
+								//文字列にins_noが含まれる場合は追加
+								$msg_no = $_REQUEST['ins_no'];
+								error_log("追加メッセージNo." . $msg_no);
+								$message = $_REQUEST['ins_msg'];
+								error_log($message);
+
+								if ($msg_no > 0 && $message != "") {
+									//webAPIにて、メッセージ更新チェック
+									$url = "http://calloncall.herokuapp.com/calling-msg-check.php?msg_no=" . $msg_no . "&message=" . $message;
+									$ch = curl_init();
+									curl_setopt($ch, CURLOPT_URL, $url);
+
+									$response = curl_exec($ch);
+									curl_close($ch);
+									error_log(print_r($response, true));
+								}
+							}
+						}
+
+            echo '<p>メッセージを更新しました。</p>';
+						echo '<ul class="actions">';
+						echo '<li><a href="admin-msg-list.php" class="button big">戻る</a></li>';
+						echo '</ul>';
+          }
+
+					?>
+				</section>
+
+			</div>
+		</div>
+
+<?php require 'admin-menu.php'; ?>
+<?php require 'footer.php'; ?>
