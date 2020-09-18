89dd22d792782c3837d1e36d997241907102b874
dmyr0613 <dmyr0613@gmail.com>
2020-09-18 21:11:40 +0900

    1

--- push_curl.php
+++ push_curl.php
@@ -1,23 +1,23 @@
 <?php
 try{
 
-	$headers = array(
-			"Accept: application/x-www-form-urlencoded",
-			"Authorization: Basic jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6:"
-	);
+	// $headers = array(
+	// 		"Accept: application/x-www-form-urlencoded",
+	// 		"Authorization: Basic jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6:"
+	// );
 
 	$options = "modelId=b8303123-3ee6-488c-823a-52a9d5e6fc8c&urls=https://commutalk.herokuapp.com/upload/001.jpg";
-
-	$options2 = array(
-			"modelId=b8303123-3ee6-488c-823a-52a9d5e6fc8c&urls=https://commutalk.herokuapp.com/upload/001.jpg",
-			"jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6:"
-	);
-
-	$headers2 = array(
-			"accept: application/x-www-form-urlencoded",
-			"modelId=b8303123-3ee6-488c-823a-52a9d5e6fc8c&urls=https://commutalk.herokuapp.com/upload/001.jpg",
-			"Authorization: Basic jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6"
-	);
+	//
+	// $options2 = array(
+	// 		"modelId=b8303123-3ee6-488c-823a-52a9d5e6fc8c&urls=https://commutalk.herokuapp.com/upload/001.jpg",
+	// 		"jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6:"
+	// );
+	//
+	// $headers2 = array(
+	// 		"accept: application/x-www-form-urlencoded",
+	// 		"modelId=b8303123-3ee6-488c-823a-52a9d5e6fc8c&urls=https://commutalk.herokuapp.com/upload/001.jpg",
+	// 		"Authorization: Basic jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6"
+	// );
 
 	$headers3 = array(
 		'Authorization: Basic ' . base64_encode("jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6:"),
@@ -26,18 +26,9 @@ try{
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, 'https://app.nanonets.com/api/v2/ImageCategorization/LabelUrls/');
-	// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Basic jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6', 'Content-Type: application/x-www-form-urlencoded'));
-	// curl_setopt($ch, CURLOPT_HTTPHEADER, "Content-Type: application/x-www-form-urlencoded Authorization: Basic jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6:");
-	// curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6", "Accept: application/x-www-form-urlencoded"));
-
-	// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	// curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type:application/x-www-form-urlencoded',]);
-
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers3);
 	curl_setopt($ch, CURLOPT_POST, 1);
-
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $options);
-	// curl_setopt($ch, CURLOPT_POSTFIELDS, $options2);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 	$response = curl_exec($ch);
