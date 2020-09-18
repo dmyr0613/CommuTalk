b10160c7f79a3a0aac08d1a8b290b8f55222b448
dmyr0613 <dmyr0613@gmail.com>
2020-09-18 21:39:22 +0900

    1

--- push_curl.php
+++ push_curl.php
@@ -20,13 +20,31 @@ try{
 
 	// $json = mb_convert_encoding($response, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 	$arr = json_decode($response,true);
+	error_log(print_r($arr, true));
 
-error_log(print_r($arr, true));
+	$json_array = array();
+
+	if ($arr === NULL) {
+	        return;
+	}else{
+	        $json_count = count($arr["result"]["prediction"]);
+					$label = array();
+	        $probability = array();
+	        for($i=$json_count-1;$i>=0;$i--){
+
+							$row_array['label'] = $arr["result"]["prediction"][$i]["label"];
+							$row_array['probability'] = $arr["result"]["prediction"][$i]["blogData"];
+
+							array_push($json_array,$row_array);
+	        }
+	}
+
+
 
 	//半分おまじない。JSONで送りますよという合図
   header("Content-Type: text/javascript; charset=utf-8");
   //JSON 形式にエンコードしてechoでPOST送信
-  echo json_encode($arr);
+  echo json_encode($json_array);
 
 //コマンドプロンプトで以下を実行してもOKシングルクオーテーションではなくダブルクォーテーションで囲む
 //curl --request POST --url "https://app.nanonets.com/api/v2/ImageCategorization/LabelUrls/" --header "accept: application/x-www-form-urlencoded" -d "modelId=b8303123-3ee6-488c-823a-52a9d5e6fc8c&urls=https://commutalk.herokuapp.com/images/strawberry.jpg" -u "jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6:"
