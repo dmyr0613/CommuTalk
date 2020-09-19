d06c9e5ff07fef4eaa22011ae8c6e0dee307f547
dmyr0613 <dmyr0613@gmail.com>
2020-09-19 23:03:38 +0900

    1

--- push_curl.php
+++ push_curl.php
@@ -45,7 +45,7 @@ try{
 	//半分おまじない。JSONで送りますよという合図
   header("Content-Type: text/javascript; charset=utf-8");
   //JSON 形式にエンコードしてechoでPOST送信
-  echo json_encode($json_array);
+  echo json_encode($json_array, JSON_UNESCAPED_UNICODE);
 
 //コマンドプロンプトで以下を実行してもOKシングルクオーテーションではなくダブルクォーテーションで囲む
 //curl --request POST --url "https://app.nanonets.com/api/v2/ImageCategorization/LabelUrls/" --header "accept: application/x-www-form-urlencoded" -d "modelId=b8303123-3ee6-488c-823a-52a9d5e6fc8c&urls=https://commutalk.herokuapp.com/images/strawberry.jpg" -u "jdMwAIdjpRQ8OnK4vv6EeTB53MwZe1z6:"
